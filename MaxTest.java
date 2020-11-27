df4bd11bcfcd9d6b7aef0416f46252c6cca30800
naveeny061 <naveeny061@gmail.com>
2020-11-27 20:24:29 +0530

    Added: Test case for max float value at first position

--- MaxTest.java
+++ MaxTest.java
@@ -6,21 +6,27 @@ import org.junit.Test;
 public class MaxTest {
 
 	@Test
-	public void givenMaxNumberAtFirstPosition_ReturnMaximumValueSame() {
+	public void givenMaxIntegrNumberAtFirstPosition_ReturnMaximumValueSame() {
 		FindMax maxValue=new FindMax();
 		Integer result =maxValue.max(9, 8, 3);
 		Assert.assertSame(9, result);
 	}
 	@Test
-	public void givenMaxNumberAtSecondPosition_ReturnMaximumValueSame() {
+	public void givenMaxIntegerNumberAtSecondPosition_ReturnMaximumValueSame() {
 		FindMax maxValue=new FindMax();
 		Integer result =maxValue.max(8,18, 3);
 		Assert.assertSame(18, result);
 	}
 	@Test
-	public void givenMaxNumberAtThirdPosition_ReturnMaximumValueSame() {
+	public void givenMaxIntegerNumberAtThirdPosition_ReturnMaximumValueSame() {
 		FindMax maxValue=new FindMax();
 		Integer result =maxValue.max(9, 8, 13);
 		Assert.assertSame(13, result);
+	}
+	@Test
+	public void givenMaxFloatNumberAtFirstPosition_ReturnMaximumValueSame() {
+		FindMax maxValue=new FindMax();
+		Float result =maxValue.max(9.12f, 8.21f, 3.214f);
+		Assert.assertEquals(9.12f, result, 0.0f);
 	}
 }
