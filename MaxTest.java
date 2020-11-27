7ce18ab25f119b394590e39239f6009fc4cb1fd9
naveeny061 <naveeny061@gmail.com>
2020-11-27 20:05:18 +0530

    Added: Test case for max at third position

--- MaxTest.java
+++ MaxTest.java
@@ -17,4 +17,10 @@ public class MaxTest {
 		Integer result =maxValue.max(8,18, 3);
 		Assert.assertSame(18, result);
 	}
+	@Test
+	public void givenMaxNumberAtThirdPosition_ReturnMaximumValueSame() {
+		FindMax maxValue=new FindMax();
+		Integer result =maxValue.max(9, 8, 13);
+		Assert.assertSame(13, result);
+	}
 }
