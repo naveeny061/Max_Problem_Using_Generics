a43398fa09e8331532738f3ad069a1a308722249
naveeny061 <naveeny061@gmail.com>
2020-11-27 21:18:43 +0530

    Added: Method to find max String lexicographically

--- FindMax.java
+++ FindMax.java
@@ -1,7 +1,5 @@
 package com.maximum;
 
-import java.util.Scanner;
-
 public class FindMax {
 	public static Integer max(Integer first, Integer second, Integer third) {
 		Integer max = first;
@@ -21,11 +19,23 @@ public class FindMax {
 		return max;
 	}
 
+	public static String max(String first, String second, String third) {
+		String max = first;
+		if (second.compareTo(max) > 0)
+			max = second;
+		if (third.compareTo(max) > 0)
+			max = third;
+		return max;
+	}
+
 	public static void main(String[] args) {
 		Integer[] intArray = { 1, 2, 3 };
 		Float[] floatArray = { 1.224f, 3.364f, 4.365f };
+		String[] stringArray = { "Apples", "Peach", "Bannana" };
+
 		System.out.println("max integer--" + max(intArray[0], intArray[1], intArray[2]));
 		System.out.println("max float--" + max(floatArray[0], floatArray[1], floatArray[2]));
+		System.out.println("max String--" + max(stringArray[0], stringArray[1], stringArray[2]));
 	}
 
 }
