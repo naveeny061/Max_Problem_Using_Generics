38b0fa6d73a0ecba3031702602735ec39b2e8ab3
naveeny061 <naveeny061@gmail.com>
2020-11-27 19:42:18 +0530

    Added: Method to find maximum integer value

--- FindMax.java
+++ FindMax.java
@@ -1,9 +1,27 @@
 package com.maximum;
 
-public class FindMax {
-	public static void main(String[] args) {
-		System.out.println("Welcome to maximum test program");
+import java.util.Scanner;
 
+public class FindMax {
+	public static Integer max(Integer first, Integer second, Integer third) {
+		Integer max = first;
+		if (second.compareTo(max) > 0)
+			max = second;
+		if (third.compareTo(max) > 0)
+			max = third;
+		return max;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Integer[] intArray = new Integer[3];
+		System.out.println("Enter first number");
+		intArray[0] = scanner.nextInt();
+		System.out.println("Enter second number");
+		intArray[1] = scanner.nextInt();
+		System.out.println("Enter third number");
+		intArray[2] = scanner.nextInt();
+		System.out.println(max(intArray[0], intArray[1], intArray[2]));
 	}
 
 }
