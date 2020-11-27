8d35d7d71b492143a790b8379e9e8f1e6413f3d8
naveeny061 <naveeny061@gmail.com>
2020-11-27 20:12:32 +0530

    Added: Method to find max float value

--- FindMax.java
+++ FindMax.java
@@ -12,16 +12,20 @@ public class FindMax {
 		return max;
 	}
 
+	public static Float max(Float first, Float second, Float third) {
+		Float max = first;
+		if (second.compareTo(max) > 0)
+			max = second;
+		if (third.compareTo(max) > 0)
+			max = third;
+		return max;
+	}
+
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		Integer[] intArray = new Integer[3];
-		System.out.println("Enter first number");
-		intArray[0] = scanner.nextInt();
-		System.out.println("Enter second number");
-		intArray[1] = scanner.nextInt();
-		System.out.println("Enter third number");
-		intArray[2] = scanner.nextInt();
-		System.out.println(max(intArray[0], intArray[1], intArray[2]));
+		Integer[] intArray = { 1, 2, 3 };
+		Float[] floatArray = { 1.224f, 3.364f, 4.365f };
+		System.out.println("max integer--" + max(intArray[0], intArray[1], intArray[2]));
+		System.out.println("max float--" + max(floatArray[0], floatArray[1], floatArray[2]));
 	}
 
 }
