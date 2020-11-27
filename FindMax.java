86151755f9561b9f584aabf18b30902bdca96d04
naveeny061 <naveeny061@gmail.com>
2020-11-27 22:52:38 +0530

    Refactor: Generic class is use

--- FindMax.java
+++ FindMax.java
@@ -1,8 +1,22 @@
 package com.maximum;
 
-public class FindMax {
-	public static <E extends Comparable<E>> E max(E first, E second, E third) {
-		E max = first;
+public class FindMax<T extends Comparable<T>> {
+	private T first,second,third;
+	
+	public FindMax(T first,T second,T third) {
+		this.first=first;
+		this.second=second;
+		this.third=third;
+	}
+	public FindMax() {
+		// TODO Auto-generated constructor stub
+	}
+	public T Max() {
+		return FindMax.testMaximum(first, second, third);
+	}
+	
+	public static <T extends Comparable<T>> T testMaximum(T first, T second, T third) {
+		T max = first;
 		if (second.compareTo(max) > 0)
 			max = second;
 		if (third.compareTo(max) > 0)
@@ -10,14 +24,15 @@ public class FindMax {
 		return max;
 	}
 
+
 	public static void main(String[] args) {
 		Integer[] intArray = { 1, 2, 3 };
 		Float[] floatArray = { 1.224f, 3.364f, 4.365f };
 		String[] stringArray = { "Apples", "Peach", "Bannana" };
-
-		System.out.println("max integer--" + max(intArray[0], intArray[1], intArray[2]));
-		System.out.println("max float--" + max(floatArray[0], floatArray[1], floatArray[2]));
-		System.out.println("max String--" + max(stringArray[0], stringArray[1], stringArray[2]));
+	
+		System.out.println("max integer--" + new FindMax<Integer>(intArray[0], intArray[1], intArray[2]).Max());
+		System.out.println("max float--" + new FindMax<Float>(floatArray[0], floatArray[1], floatArray[2]).Max());
+		System.out.println("max String--" + new FindMax<String>(stringArray[0], stringArray[1], stringArray[2]).Max());
 	}
 
 }
--- MaxTest.java
+++ MaxTest.java
@@ -8,55 +8,55 @@ public class MaxTest {
 	@Test
 	public void givenMaxIntegrNumberAtFirstPosition_ReturnMaximumValueSame() {
 		FindMax maxValue=new FindMax();
-		Integer result =maxValue.max(9, 8, 3);
+		Integer result =maxValue.testMaximum(9, 8, 3);
 		Assert.assertSame(9, result);
 	}
 	@Test
 	public void givenMaxIntegerNumberAtSecondPosition_ReturnMaximumValueSame() {
 		FindMax maxValue=new FindMax();
-		Integer result =maxValue.max(8,18, 3);
+		Integer result =maxValue.testMaximum(8,18, 3);
 		Assert.assertSame(18, result);
 	}
 	@Test
 	public void givenMaxIntegerNumberAtThirdPosition_ReturnMaximumValueSame() {
 		FindMax maxValue=new FindMax();
-		Integer result =maxValue.max(9, 8, 13);
+		Integer result =maxValue.testMaximum(9, 8, 13);
 		Assert.assertSame(13, result);
 	}
 	@Test
 	public void givenMaxFloatNumberAtFirstPosition_ReturnMaximumValueSame() {
 		FindMax maxValue=new FindMax();
-		Float result =maxValue.max(9.12f, 8.21f, 3.214f);
+		Float result =maxValue.testMaximum(9.12f, 8.21f, 3.214f);
 		Assert.assertEquals(9.12f, result, 0.0f);
 	}
 	@Test
 	public void givenMaxFloatNumberAtSecondPosition_ReturnMaximumValueSame() {
 		FindMax maxValue=new FindMax();
-		Float result =maxValue.max(9.12f, 18.21f, 3.214f);
+		Float result =maxValue.testMaximum(9.12f, 18.21f, 3.214f);
 		Assert.assertEquals(18.21f, result, 0.0f);
 	}
 	@Test
 	public void givenMaxFloatNumberAtThirdPosition_ReturnMaximumValueSame() {
 		FindMax maxValue=new FindMax();
-		Float result =maxValue.max(9.12f, 18.21f, 31.214f);
+		Float result =maxValue.testMaximum(9.12f, 18.21f, 31.214f);
 		Assert.assertEquals(31.214f, result, 0.0f);
 	}
 	@Test
 	public void givenMaxStringAtFirstPosition_ReturnMaximumValueSame() {
 		FindMax maxValue=new FindMax();
-		String result =maxValue.max("Peach","Apple","Bannana");
+		String result =maxValue.testMaximum("Peach","Apple","Bannana");
 		Assert.assertEquals("Peach", result);
 	}
 	@Test
 	public void givenMaxStringAtSecondPosition_ReturnMaximumValueSame() {
 		FindMax maxValue=new FindMax();
-		String result =maxValue.max("Apple","Peach","Bannana");
+		String result =maxValue.testMaximum("Apple","Peach","Bannana");
 		Assert.assertEquals("Peach", result);
 	}
 	@Test
 	public void givenMaxStringAtThirdPosition_ReturnMaximumValueSame() {
 		FindMax maxValue=new FindMax();
-		String result =maxValue.max("Bannana","Apple","Peach");
+		String result =maxValue.testMaximum("Bannana","Apple","Peach");
 		Assert.assertEquals("Peach", result);
 	}
 }
