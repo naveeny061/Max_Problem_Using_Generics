a2090acf6e626c7201f03b77dad4de9b36f6785f
naveeny061 <naveeny061@gmail.com>
2020-11-30 16:13:22 +0530

    Added: Print Method to print in std out

--- FindMax.java
+++ FindMax.java
@@ -28,21 +28,22 @@ public class FindMax<T extends Comparable<T>> {
 			max = third;
 		if (fourth.compareTo(max) > 0)
 			max = fourth;
+		printMax(first, second, third, fourth, max);
 		return max;
+	}
+
+	public static <T> void printMax(T first, T second, T third, T fourth, T max) {
+		System.out.printf("Max of %s, %s, %s and %s is %s\n", first, second, third, fourth, max);
 	}
 
 	public static void main(String[] args) {
 		Integer[] intArray = { 1, 2, 3, 4 };
 		Float[] floatArray = { 1.224f, 3.364f, 4.365f, 2.134f };
 		String[] stringArray = { "Apples", "Peach", "Bannana", "Orange" };
-		
-		
-		System.out.println(
-				"max integer--" + new FindMax<Integer>(intArray[0], intArray[1], intArray[2], intArray[3]).Max());
-		System.out.println(
-				"max float--" + new FindMax<Float>(floatArray[0], floatArray[1], floatArray[2], floatArray[3]).Max());
-		System.out.println("max String--"
-				+ new FindMax<String>(stringArray[0], stringArray[1], stringArray[2], stringArray[3]).Max());
+
+		new FindMax<Integer>(intArray[0], intArray[1], intArray[2], intArray[3]).Max();
+		new FindMax<Float>(floatArray[0], floatArray[1], floatArray[2], floatArray[3]).Max();
+		new FindMax<String>(stringArray[0], stringArray[1], stringArray[2], stringArray[3]).Max();
 	}
 
 }
