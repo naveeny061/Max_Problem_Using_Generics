abb617a82078dbaa34b4d94073336de336a444e3
naveeny061 <naveeny061@gmail.com>
2020-11-30 16:01:46 +0530

    Refactor: To pass four parameters

--- FindMax.java
+++ FindMax.java
@@ -1,38 +1,48 @@
 package com.maximum;
 
+import java.util.Arrays;
+
 public class FindMax<T extends Comparable<T>> {
-	private T first,second,third;
-	
-	public FindMax(T first,T second,T third) {
-		this.first=first;
-		this.second=second;
-		this.third=third;
+	private T first, second, third, fourth;
+
+	public FindMax(T first, T second, T third, T fourth) {
+		this.first = first;
+		this.second = second;
+		this.third = third;
+		this.fourth = fourth;
 	}
+
 	public FindMax() {
 		// TODO Auto-generated constructor stub
 	}
+
 	public T Max() {
-		return FindMax.testMaximum(first, second, third);
+		return FindMax.Maximum(first, second, third, fourth);
 	}
-	
-	public static <T extends Comparable<T>> T testMaximum(T first, T second, T third) {
+
+	public static <T extends Comparable<T>> T Maximum(T first, T second, T third, T fourth) {
 		T max = first;
 		if (second.compareTo(max) > 0)
 			max = second;
 		if (third.compareTo(max) > 0)
 			max = third;
+		if (fourth.compareTo(max) > 0)
+			max = fourth;
 		return max;
 	}
 
-
 	public static void main(String[] args) {
-		Integer[] intArray = { 1, 2, 3 };
-		Float[] floatArray = { 1.224f, 3.364f, 4.365f };
-		String[] stringArray = { "Apples", "Peach", "Bannana" };
-	
-		System.out.println("max integer--" + new FindMax<Integer>(intArray[0], intArray[1], intArray[2]).Max());
-		System.out.println("max float--" + new FindMax<Float>(floatArray[0], floatArray[1], floatArray[2]).Max());
-		System.out.println("max String--" + new FindMax<String>(stringArray[0], stringArray[1], stringArray[2]).Max());
+		Integer[] intArray = { 1, 2, 3, 4 };
+		Float[] floatArray = { 1.224f, 3.364f, 4.365f, 2.134f };
+		String[] stringArray = { "Apples", "Peach", "Bannana", "Orange" };
+		
+		
+		System.out.println(
+				"max integer--" + new FindMax<Integer>(intArray[0], intArray[1], intArray[2], intArray[3]).Max());
+		System.out.println(
+				"max float--" + new FindMax<Float>(floatArray[0], floatArray[1], floatArray[2], floatArray[3]).Max());
+		System.out.println("max String--"
+				+ new FindMax<String>(stringArray[0], stringArray[1], stringArray[2], stringArray[3]).Max());
 	}
 
 }
--- MaxTest.java
+++ MaxTest.java
@@ -7,56 +7,85 @@ public class MaxTest {
 
 	@Test
 	public void givenMaxIntegrNumberAtFirstPosition_ReturnMaximumValueSame() {
-		FindMax maxValue=new FindMax();
-		Integer result =maxValue.testMaximum(9, 8, 3);
+		FindMax maxValue = new FindMax();
+		Integer result = maxValue.Maximum(9, 8, 3, 6);
 		Assert.assertSame(9, result);
 	}
+
 	@Test
 	public void givenMaxIntegerNumberAtSecondPosition_ReturnMaximumValueSame() {
-		FindMax maxValue=new FindMax();
-		Integer result =maxValue.testMaximum(8,18, 3);
+		FindMax maxValue = new FindMax();
+		Integer result = maxValue.Maximum(8, 18, 3, 6);
 		Assert.assertSame(18, result);
 	}
+
 	@Test
 	public void givenMaxIntegerNumberAtThirdPosition_ReturnMaximumValueSame() {
-		FindMax maxValue=new FindMax();
-		Integer result =maxValue.testMaximum(9, 8, 13);
+		FindMax maxValue = new FindMax();
+		Integer result = maxValue.Maximum(9, 8, 13, 6);
 		Assert.assertSame(13, result);
 	}
+
+	@Test
+	public void givenMaxIntegerNumberAtFourthPosition_ReturnMaximumValueSame() {
+		FindMax maxValue = new FindMax();
+		Integer result = maxValue.Maximum(9, 8, 6, 13);
+		Assert.assertSame(13, result);
+	}
+
 	@Test
 	public void givenMaxFloatNumberAtFirstPosition_ReturnMaximumValueSame() {
-		FindMax maxValue=new FindMax();
-		Float result =maxValue.testMaximum(9.12f, 8.21f, 3.214f);
+		FindMax maxValue = new FindMax();
+		Float result = maxValue.Maximum(9.12f, 8.21f, 3.214f, 3.32f);
 		Assert.assertEquals(9.12f, result, 0.0f);
 	}
+
 	@Test
 	public void givenMaxFloatNumberAtSecondPosition_ReturnMaximumValueSame() {
-		FindMax maxValue=new FindMax();
-		Float result =maxValue.testMaximum(9.12f, 18.21f, 3.214f);
+		FindMax maxValue = new FindMax();
+		Float result = maxValue.Maximum(9.12f, 18.21f, 3.214f, 3.32f);
 		Assert.assertEquals(18.21f, result, 0.0f);
 	}
+
 	@Test
 	public void givenMaxFloatNumberAtThirdPosition_ReturnMaximumValueSame() {
-		FindMax maxValue=new FindMax();
-		Float result =maxValue.testMaximum(9.12f, 18.21f, 31.214f);
+		FindMax maxValue = new FindMax();
+		Float result = maxValue.Maximum(9.12f, 18.21f, 31.214f, 3.32f);
 		Assert.assertEquals(31.214f, result, 0.0f);
 	}
+
+	@Test
+	public void givenMaxFloatNumberAtFourthPosition_ReturnMaximumValueSame() {
+		FindMax maxValue = new FindMax();
+		Float result = maxValue.Maximum(9.12f, 18.21f, 3.32f, 31.214f);
+		Assert.assertEquals(31.214f, result, 0.0f);
+	}
+
 	@Test
 	public void givenMaxStringAtFirstPosition_ReturnMaximumValueSame() {
-		FindMax maxValue=new FindMax();
-		String result =maxValue.testMaximum("Peach","Apple","Bannana");
+		FindMax maxValue = new FindMax();
+		String result = maxValue.Maximum("Peach", "Apple", "Bannana", "Orrange");
 		Assert.assertEquals("Peach", result);
 	}
+
 	@Test
 	public void givenMaxStringAtSecondPosition_ReturnMaximumValueSame() {
-		FindMax maxValue=new FindMax();
-		String result =maxValue.testMaximum("Apple","Peach","Bannana");
+		FindMax maxValue = new FindMax();
+		String result = maxValue.Maximum("Apple", "Peach", "Bannana", "Orrange");
 		Assert.assertEquals("Peach", result);
 	}
+
 	@Test
 	public void givenMaxStringAtThirdPosition_ReturnMaximumValueSame() {
-		FindMax maxValue=new FindMax();
-		String result =maxValue.testMaximum("Bannana","Apple","Peach");
+		FindMax maxValue = new FindMax();
+		String result = maxValue.Maximum("Bannana", "Apple", "Peach", "Orrange");
+		Assert.assertEquals("Peach", result);
+	}
+
+	@Test
+	public void givenMaxStringAtFouthPosition_ReturnMaximumValueSame() {
+		FindMax maxValue = new FindMax();
+		String result = maxValue.Maximum("Bannana", "Apple", "Orrange", "Peach");
 		Assert.assertEquals("Peach", result);
 	}
 }
